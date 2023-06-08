72f253f25c6cdcca1ed1d172b7e95e3badaa9952
Falk358 <maxdanelli@gmail.com>
2023-06-08 20:50:50 +0200

    cleaned up some config; disabled yank to clipboard because it doesnt work

--- keymaps.lua
+++ keymaps.lua
@@ -5,10 +5,14 @@ vim.keymap.set("v", "K", ":m '<r2<CR>gv=gv")
 -- keep cursor in middle when jumping with ctrl-u ctrl-d
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+-- when pasting with leader p, we dont replace the current vim register with the value we are overwriting
+vim.keymap.set("x", "<leader>p", "\"_dP")
+
 -- keeps curser centered when scrolling through search matches
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
--- copy to system clipboard instead of vims own
-vim.keymap.set("n","<leader>y", "\"+y")
-vim.keymap.set("v","<leader>y", "\"+y")
-vim.keymap.set("n","<leader>Y", "\"+Y")
+-- copy to system clipboard 
+--vim.keymap.set("n","<leader>y", "\"+y")
+--vim.keymap.set("v","<leader>y", "\"+y")
+--vim.keymap.set("n","<leader>Y", "\"+Y")
