b5cc57481b49885affaddafa9dc60513978862af
Falk358 <maxdanelli@gmail.com>
2023-06-12 22:02:24 +0200

    fixed <leader>p command

--- keymaps.lua
+++ keymaps.lua
@@ -1,4 +1,3 @@
-vim.g.mapleader = " "
 -- allows marked lines to be moved around in visual mode
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<r2<CR>gv=gv")
@@ -9,7 +8,7 @@ vim.keymap.set("n", "<C-u>", "<C-u>zz")
 -- when pasting with leader p, we dont replace the current vim register with the value we are overwriting
 vim.keymap.set("x", "<leader>p", "\"_dP")
 
--- keeps curser centered when scrolling through search matches
+-- keeps cursor centered when scrolling through search matches
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 -- copy to system clipboard 
