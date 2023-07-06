3079398e9c4807d05e34225f04ab9ee0a184942d
Falk358 <maxdanelli@gmail.com>
2023-07-06 21:43:44 +0200

    added flash.nvim plugin; jump to character navigation

--- init.lua
+++ init.lua
@@ -141,6 +141,60 @@ require('lazy').setup({
       vim.api.nvim_set_hl(0, "NormalFloat", {bg = "none"}) -- set transparent background
     end,
   },
+  { -- plugin for fast character search of first two chars of word, lets you jump to the word
+    "folke/flash.nvim",
+    event = "VeryLazy",
+    ---@type Flash.Config
+    opts = {},
+    keys = {
+      {
+        "s",
+        mode = { "n", "x", "o" },
+        function()
+          require("flash").jump({
+            search = {
+              mode = function(str)
+                return "\\<" .. str
+              end,
+            },
+          })
+        end,
+        desc = "Flash",
+      },
+      {
+        "S",
+        mode = { "n", "o", "x" },
+        function()
+          require("flash").treesitter()
+        end,
+        desc = "Flash Treesitter",
+      },
+      {
+        "r",
+        mode = "o",
+        function()
+          require("flash").remote()
+        end,
+        desc = "Remote Flash",
+      },
+      {
+        "R",
+        mode = { "o", "x" },
+        function()
+          require("flash").treesitter_search()
+        end,
+        desc = "Flash Treesitter Search",
+      },
+      {
+        "<c-s>",
+        mode = { "c" },
+        function()
+          require("flash").toggle()
+        end,
+        desc = "Toggle Flash Search",
+      },
+    },
+  },
 
   {
     -- Set lualine as statusline
